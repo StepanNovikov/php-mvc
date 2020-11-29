77d95fcdc51a55ced33310c7247b85a3f925c5f2
Stepan <stepan.novikov.1996.com>
2020-11-29 12:18:00 +0300

    added router class and file routes

---
+++ application/config/routes.php
@@ -0,0 +1,15 @@
+<?php
+    return [
+        'PHP%20MVC/account/login' => [
+            'controller' => 'account',
+            'action'=>'login',
+        ],
+        'PHP%20MVC' => [
+            'controller' => 'main',
+            'action'=>'index',
+        ],
+        'PHP%20MVC/news/show' => [
+            'controller' => 'news',
+            'action' => 'show',
+        ]
+    ];
--- application/core/Router.php
+++ application/core/Router.php
@@ -8,19 +8,43 @@
 
         function __construct()
         {
-            //
+            $arr = require 'application/config/routes.php';
+            foreach ($arr as $key => $value){
+                $this->add($key,$value);
+            }
         }
 
-        public function add(){
-            //
+        public function add($route, $params){
+            $route = '#^'.$route.'$#';
+            $this->routes[$route] = $params;
+
         }
 
         public function match(){
-            //
+            $url = $_SERVER['REQUEST_URI'];
+            $url = trim($url,'/');
+
+            foreach ($this->routes as $route => $params){
+
+                if(preg_match($route,$url,$matches)){
+                    $this->params = $params;
+                    return true;
+                }
+            }
+            return false;
         }
 
         public function run(){
-            echo 'start';
+            if($this->match()){
+                $controller = 'application\controllers\\'.ucfirst($this->params['controller']).'.php';
+                if(class_exists($controller)){
+                    echo 'OK';
+                } else{
+                    echo 'Класс '.ucfirst($this->params['controller']).' не найден';
+                }
+            } else {
+                echo 'Маршрут не найден';
+            }
         }
 
 
